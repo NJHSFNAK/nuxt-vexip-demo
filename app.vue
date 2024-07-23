05cf1923eb34010cc1ab9c3c0fc6c01ad47be10f
韩小煦 <18232241431@163.com>
2024-07-23 13:36:28 +0800

    init: 项目上传

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss">
+@use "vexip-ui/style";
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index">
+    <VImage
+      width="200"
+      height="120"
+      src="https://www.vexipui.com/picture-1.jpg"
+    ></VImage>
+    <VImage
+      width="200"
+      height="120"
+      skeleton
+      src="https://www.vexipui.com/picture-1.jpg"
+    ></VImage>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
